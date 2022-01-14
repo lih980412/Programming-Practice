c5ff50f186067d5bdfb8893efd9a42c2aabccd4d
lh <739718081@qq.com>
2022-01-15 00:27:39 +0800

    2 push

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,14 +1,12 @@
-nums = [1,1,2]
+nums = [1, 1, 2]
 
 length = len(nums)
 fast = 1
 slow = 0
-while fast<length:
-    if nums[fast] == nums[fast-1]:
+while fast < length:
+    if nums[fast] == nums[fast - 1]:
         nums[slow] = nums[fast]
         slow += 1
     fast += 1
 str = "112"
 str.find("2")
-
-print(11)
